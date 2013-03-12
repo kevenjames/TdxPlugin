81679330d41ea972fdf170519bb778493449b08c
Tim Lei <kevenjames2000@hotmail.com>
2013-03-12 17:54:19 +0800

    change log.h

--- Log.h
+++ Log.h
@@ -5,16 +5,33 @@
 #define LOG_FILE_PATH "D:\\new_zszq\\Log\DebugInfo.log"
 
 #ifdef TEST
-
+	#define LogData Logger::Instance().log
+	#define SetLogFile Logger::SetLogFilePath
 #else
+	#define LogData
+	#define SetLogFile
 #endif
+
+using namespace std;
 
 namespace TdxPlugin 
 {
+	typedef enum tagLogLevel
+	{
+		LOG_TRACE = 0,
+		LOG_INFO,
+		LOG_WARNING,
+		LOG_FATAL,
+		LOG_NONE,
+	}LogLevel
+
 	class CLog
 	{
+	public:
+		static CLog& Instance();
+		static void SetLogFilePath(const string& strFilePath, bool bAppend);
 	
-	}
+	};
 }
 
 #endif //_LOG_H
